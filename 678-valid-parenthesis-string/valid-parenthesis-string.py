16015e2189d27cc50d4246ed7f4c49dc6b5d5381
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-01-18 17:40:01 -0600

    Time: 7 ms (11.86%) | Memory: 19.5 MB (8.43%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,22 +1,29 @@
-class Solution:
-    def checkValidString(self, s: str) -> bool:
-        min_cnt, max_cnt = 0, 0
+from collections import deque
 
-        for c in s:
-            if c=="(":
-                min_cnt+=1
-                max_cnt+=1
-            elif c==")":
-                min_cnt-=1
-                if min_cnt < 0 and max_cnt > 0:
-                    min_cnt = 0
-                max_cnt-=1
-            else:
-                min_cnt = max(0, min_cnt-1)
-                max_cnt+=1
-            
-            
-            if min_cnt < 0 and max_cnt < 0:
-                return False
+class Solution:
+    def isBalanced(self, s, idx, cnt, dp):
+        n =len(s)
+        if cnt < 0:
+            dp[idx][cnt+n] = False
+            return dp[idx][cnt+n]
         
-        return 0==min_cnt
+        if idx == len(s):
+            dp[idx][cnt+n] = (cnt==0)
+            return dp[idx][cnt+n]
+        
+        if dp[idx][cnt+n] is not None:
+            return dp[idx][cnt+n] 
+
+        if s[idx]=="(":
+            dp[idx][cnt+n] = self.isBalanced(s, idx+1, cnt+1, dp)
+        elif s[idx]==")":
+            dp[idx][cnt+n] = self.isBalanced(s, idx+1, cnt-1, dp)
+        else:
+            dp[idx][cnt+n] = (self.isBalanced(s, idx+1, cnt+1, dp) or self.isBalanced(s, idx+1, cnt-1, dp) or self.isBalanced(s, idx+1, cnt, dp))
+        
+        return dp[idx][cnt+n] 
+
+    def checkValidString(self, s: str) -> bool:
+        idx, cnt = 0, 0
+        dp = [[None]*(2*len(s)+1)  for _ in range(len(s)+1)]
+        return self.isBalanced(s, idx, cnt, dp)
