3e19769d2b9e24ff9ec031ea33cdab5f4237beee
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-02-08 14:24:04 -0600

    Time: 37 ms (15.28%) | Memory: 22 MB (18.36%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,30 +1,20 @@
 class Solution:
-    def get_min_path_sum(self, a, ii, jj, dp):
-        if ii==len(a)-1 and jj==len(a[0])-1:
+    def minPathFromCurrPt(self, a, ii, jj, dp):
+        if (ii==(len(a)-1)) and (jj==len(a[0])-1):
             dp[ii][jj] = a[ii][jj]
-            return a[ii][jj]
+            return dp[ii][jj]
 
-        if ii > len(a)-1 or jj > len(a[0])-1:
+        if (ii >= len(a)) or (jj >= len(a[0])):
             return float('inf')
         
         if dp[ii][jj]!=-1:
             return dp[ii][jj]
         
-        dp[ii][jj] = a[ii][jj] + min(self.get_min_path_sum(a, ii+1, jj, dp), self.get_min_path_sum(a, ii, jj+1, dp))
+        dp[ii][jj] = a[ii][jj] + min(self.minPathFromCurrPt(a, ii+1, jj, dp), self.minPathFromCurrPt(a, ii, jj+1, dp))
 
         return dp[ii][jj]
 
     def minPathSum(self, a: List[List[int]]) -> int:
-        m, n = len(a), len(a[0])
-        dp = [-1]*n
-        dp[n-1] = a[m-1][n-1]
-
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                if i==m-1 and j==n-1:
-                    continue
-                right = dp[j+1] if j+1 < n else float('inf')
-                down = dp[j] if i+1 < m else float('inf')
-                dp[j] = a[i][j] + min(right, down)
-
-        return dp[0]
+        dp = [[-1]*len(a[0]) for _ in range(len(a))]
+        return self.minPathFromCurrPt(a, 0, 0, dp)
+        
