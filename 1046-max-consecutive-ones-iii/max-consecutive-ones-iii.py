4ea159d640ab67605c6fc4ef30805c8677a9b80c
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-03-12 16:54:08 -0500

    Time: 71 ms (21.46%) | Memory: 22.1 MB (96.00%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -4,20 +4,17 @@ class Solution:
         n = len(a)
 
         while r < n and l <= r:
-            if a[r]==0:
+            if a[r] == 0:
                 zeros += 1
-            
+
             if zeros > k:
-                while l < n and a[l]==1:
-                    l += 1
-                if l >= n:
-                    break
-                zeros -= 1
+                if a[l] == 0:
+                    zeros -= 1
                 l += 1
             
-            curr_len = (r-l)+1
-            res = max(res, curr_len)
-
-            r += 1
+            if zeros <= k:
+                res = max(res, r-l+1)
+            
+            r += 1   
 
         return res
