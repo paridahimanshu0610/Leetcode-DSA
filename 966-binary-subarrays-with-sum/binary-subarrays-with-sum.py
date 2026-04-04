ffb239514e87eb7a9a2c5e6eb8be1f95f7682735
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-04-04 00:22:35 -0500

    Time: 55 ms (5.41%) | Memory: 19.9 MB (95.48%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,26 +1,24 @@
 class Solution:
-    def getCount(self, a, target):
-        if target < 0:
+    def getSubarray(self, a, k):
+        if k < 0:
             return 0
-            
-        l, r = 0, 0
         res = 0
+        l, r = 0, 0
         curr_sum = 0
 
         while l<=r and r<len(a):
             curr_sum += a[r]
-            if curr_sum <= target:
+            if curr_sum <= k:
                 res += (r-l+1)
             else:
-                while l < r and curr_sum > target:
+                while l < r and curr_sum > k:
                     curr_sum -= a[l]
                     l += 1
-                if l <= r and curr_sum == target:
+                if curr_sum <= k:
                     res += (r-l+1)
             r += 1
 
         return res
 
-    def numSubarraysWithSum(self, a: List[int], target: int) -> int:
-        return self.getCount(a, target) - self.getCount(a, target-1)
-                    
+    def numSubarraysWithSum(self, a: List[int], k: int) -> int:
+        return self.getSubarray(a, k) - self.getSubarray(a, k-1)
