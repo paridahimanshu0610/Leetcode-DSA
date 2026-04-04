c17c5c5cd265b7dbf125113662fc6f62751d2b3c
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-04-04 01:16:54 -0500

    Time: 187 ms (60.29%) | Memory: 26 MB (45.97%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -7,13 +7,15 @@ class Solution:
         while l <= r and r < len(a):
             freq[a[r]] = freq.get(a[r], 0) + 1
 
-            while len(freq) > 2 and l < r:
+            if len(freq) > 2:
                 freq[a[l]] -= 1
                 if freq[a[l]]==0:
                     del freq[a[l]]
                 l += 1
-            
-            res = max(res, r-l+1)
+
+            if len(freq) <= 2: 
+                res = max(res, r-l+1)
+                
             r += 1
 
         return res
