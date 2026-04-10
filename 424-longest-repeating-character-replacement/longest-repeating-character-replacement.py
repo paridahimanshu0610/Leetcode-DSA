b3c2cb9ca08f06e58ead2a9798586ea76ff7a24e
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-04-09 19:08:16 -0500

    Time: 151 ms (22.75%) | Memory: 19.7 MB (63.61%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,24 +1,24 @@
 class Solution:
-    def get_highest_frequency(self, freq):
-        return sorted(freq.items(), key=lambda x:x[1], reverse=True)[0]
-
     def characterReplacement(self, s: str, k: int) -> int:
+        freq = {}
         l, r = 0, 0
         res = 0
-        freq = {}
-        max_freq = 0
 
         while l <= r and r < len(s):
-            freq[s[r]] = freq.get(s[r], 0) + 1
-            item, f = self.get_highest_frequency(freq)
-            n = (r-l+1)
-            if (n-f) <= k:
-                res = max(res, n)
-            else:
+            freq[s[r]] = freq.get(s[r], 0)+1
+
+            max_freq = max(freq.values())
+            curr_window_len = r-l+1
+
+            while (curr_window_len - max_freq) > k:
                 freq[s[l]] -= 1
-                if freq[s[l]]==0:
+                if freq[s[l]] == 0:
                     del freq[s[l]]
                 l += 1
+                curr_window_len = r-l+1
+                max_freq = max(freq.values())
+
+            res = max(res, curr_window_len)
             r += 1
 
         return res 
