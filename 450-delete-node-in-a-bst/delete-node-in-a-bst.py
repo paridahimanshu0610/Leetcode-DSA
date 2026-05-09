b1eadf9c289e0aa9dc773ea32bd62dea4f5731bc
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-08 20:33:59 -0500

    Time: 0 ms (100.00%) | Memory: 22.1 MB (42.89%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -5,41 +5,36 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def getRightMostNode(self, node):
-        while node and node.right:
-            node = node.right
-        return node
+    def findMinVal(self, root):
+        if root is None:
+            return float('inf')
 
-    def shuffle(self, node):
-        if node.left is None:
-            return node.right
-        elif node.right is None:
-            return node.left
-        else:
-            rightmostNode = self.getRightMostNode(node.left)
-            rightmostNode.right = node.right
-            node.right = None
-            return node.left
+        res = root.val
+        while root.left:
+            root = root.left
+            res = root.val
+        
+        return res
 
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if root is None:
-            return None
-        
-        if root.val == key:
-            return self.shuffle(root)
-
-        node = root
-        while node:
-            leftPtr, rightPtr = node.left, node.right
-
-            if leftPtr and leftPtr.val==key:
-                node.left = self.shuffle(leftPtr)
-            elif rightPtr and rightPtr.val==key:
-                node.right = self.shuffle(rightPtr)
-            
-            if node.val > key:
-                node = node.left
+            return root
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if root.left is None and root.right is None:
+                return None
+            elif root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
             else:
-                node = node.right
+                minVal = self.findMinVal(root.right)
+                root.val = minVal
+                root.right = self.deleteNode(root.right, minVal)
+                return root
 
         return root
+            
