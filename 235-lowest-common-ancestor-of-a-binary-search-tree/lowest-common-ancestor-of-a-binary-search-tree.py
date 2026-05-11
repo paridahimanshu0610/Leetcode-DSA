2b7bf9e8e212171cdd059c92b62a3c710346c0c4
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-10 21:21:16 -0500

    Time: 68 ms (37.63%) | Memory: 22.7 MB (43.61%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,37 +6,13 @@
 #         self.right = None
 
 class Solution:
-    def traverse(self, node, target, path):
-        if node is None:
-            return False
-        
-        path.append(node)
-
-        if node == target:
-            return True
-        
-        if self.traverse(node.left, target, path) or self.traverse(node.right, target, path):
-            return True
-        
-        path.pop()
-
-        return False
-
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        path1, path2 = [], []
-        self.traverse(root, p, path1)
-        self.traverse(root, q, path2)
+        if root is None:
+            return None
 
-        i, j = 1, 1
-        res = root
-
-        while i < len(path1) and j < len(path2):
-            if path1[i]==path2[j]:
-                res = path1[i]
-                i += 1
-                j += 1
-            else:
-                break
-
-        return res
-            
+        if (root.val > p.val) and (root.val > q.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif (root.val < p.val) and (root.val < q.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+        
+        return root
