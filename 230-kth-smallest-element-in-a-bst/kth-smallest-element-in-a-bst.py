4cc15d5f63558f625df658eb931544fb5d26ebfc
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-10 20:18:56 -0500

    Time: 3 ms (31.01%) | Memory: 22.1 MB (60.83%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -5,25 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def in_order_traversal(self, node, k, curr_at, curr_val):
-        if (node is None) or (curr_at==k):
-            return (curr_at, curr_val)
+    def traverse(self, root, arr):
+        if root is None:
+            return
         
-        (curr_at, curr_val) = self.in_order_traversal(node.left, k, curr_at, curr_val)
-        # print("after left", node.val, curr_at, curr_val)
+        self.traverse(root.left, arr)
+        arr.append(root.val)
+        self.traverse(root.right, arr)
         
-        if curr_at!=k:
-            curr_at += 1
-            curr_val = node.val
-        else:
-            return (curr_at, curr_val) 
-        # print("after update", node.val, curr_at, curr_val)
-        
-        (curr_at, curr_val) = self.in_order_traversal(node.right, k, curr_at, curr_val)
-        # print("after right", node.val, curr_at, curr_val)
-
-        return (curr_at, curr_val)
-
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        (_, curr_val) = self.in_order_traversal(root, k, 0, None)
-        return curr_val
+        arr = []
+        self.traverse(root, arr)
+
+        return arr[k-1]       
