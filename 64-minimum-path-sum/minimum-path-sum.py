5f9d58919caae42d23bd89f45838155357f6576c
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-12 16:51:00 -0500

    Time: 7 ms (93.52%) | Memory: 20.2 MB (98.49%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,32 +1,15 @@
 class Solution:
-    def minPathFromCurrPt(self, a, ii, jj, dp):
-        if (ii==(len(a)-1)) and (jj==len(a[0])-1):
-            dp[ii][jj] = a[ii][jj]
-            return dp[ii][jj]
-
-        if (ii >= len(a)) or (jj >= len(a[0])):
-            return float('inf')
-        
-        if dp[ii][jj]!=-1:
-            return dp[ii][jj]
-        
-        dp[ii][jj] = a[ii][jj] + min(self.minPathFromCurrPt(a, ii+1, jj, dp), self.minPathFromCurrPt(a, ii, jj+1, dp))
-
-        return dp[ii][jj]
-
     def minPathSum(self, a: List[List[int]]) -> int:
         m, n = len(a), len(a[0])
-        dp = [-1]*n
-        dp[n-1] = a[m-1][n-1]
 
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                if (i==m-1) and (j==n-1):
-                    continue
-                right = dp[j+1] if j+1 < n else float('inf') 
-                down = dp[j] if i+1 < m else float('inf')
+        dp = [0]*n
+        dp[0] = a[0][0]
 
-                dp[j] = a[i][j] + min(right, down)
+        for i in range(1, n):
+            dp[i] = a[0][i] + dp[i-1]
 
-        return dp[0]
-        
+        for i in range(1, m):
+            for j in range(n):
+                dp[j] = a[i][j] + dp[j] if j == 0 else a[i][j] + min(dp[j-1], dp[j])
+
+        return dp[-1]
