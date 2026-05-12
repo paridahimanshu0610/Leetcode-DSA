8940a0607e9605bdf938b8bee8620f58f3b328b0
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-12 15:49:14 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (74.78%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,19 +1,17 @@
 class Solution:
-    def getTotalPaths(self, m, n, ii, jj, dp):
-        if ii == 0 or jj == 0:
-            dp[ii][jj] = 1
+    def walk(self, m, n, i, j, dp):
+        if i == 0 or j== 0:
+            dp[i][j] = 1
             return 1
+        
+        if dp[i][j] != -1:
+            return dp[i][j]
 
-        if ii < 0 or jj < 0:
-            return 0
+        dp[i][j] = self.walk(m, n, i-1, j, dp) + self.walk(m, n, i, j-1, dp)
+        
+        return dp[i][j] 
 
-        if dp[ii][jj]!=-1:
-            return dp[ii][jj]
-
-        dp[ii][jj] = self.getTotalPaths(m, n, ii-1, jj, dp) + self.getTotalPaths(m, n, ii, jj-1, dp)
-
-        return dp[ii][jj]
-             
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[-1]*n for _ in range(m)]
-        return self.getTotalPaths(m, n, m-1, n-1, dp)
+
+        return self.walk(m, n, m-1, n-1, dp)
