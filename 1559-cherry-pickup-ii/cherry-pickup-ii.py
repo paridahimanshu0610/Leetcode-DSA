e9e96c065f226f1ed61c886d720e629a50d05b65
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-12 17:47:47 -0500

    Time: 1039 ms (44.32%) | Memory: 26.6 MB (72.31%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -1,47 +1,36 @@
 class Solution:
-    def max_cherry(self, a, i, j1, j2, dp):
-        if i==len(a)-1 and (0<=j1<len(a[0])) and (0<=j2<len(a[0])):
-            if j1==j2:
+    def totalFromCurrPointOnwards(self, a, i, j1, j2, dp):
+        m, n = len(a), len(a[0])
+
+        if (j1 < 0 or j1 >= n) or (j2 < 0 or j2 >= n):
+            return -float('inf')
+
+        if i == m-1:
+            if j1 == j2:
                 dp[i][j1][j2] = a[i][j1]
             else:
                 dp[i][j1][j2] = a[i][j1] + a[i][j2]
 
-        if (i>=len(a)) or (j1<0 or j1>=len(a[0])) or (j2<0 or j2>=len(a[0])):
-            return float('-inf')
-
-        if dp[i][j1][j2]!=-1:
             return dp[i][j1][j2]
 
-        nxt_state_max_gain = float('-inf')
-        for dj1 in range(-1,2,1):
-            for dj2 in range(-1,2,1):
-                nxt_state_max_gain = max(nxt_state_max_gain, self.max_cherry(a, i+1, j1+dj1, j2+dj2, dp))
-        curr_state_gain = a[i][j1] + a[i][j2] if j1!=j2 else a[i][j1]
+        if dp[i][j1][j2] != -1:
+            return dp[i][j1][j2]
 
-        dp[i][j1][j2] = curr_state_gain + nxt_state_max_gain 
+        maxi = 0
+        for dj1 in range(-1, 2):
+            for dj2 in range(-1, 2):
+                jj1, jj2 = j1+dj1, j2+dj2
+                if j1 == j2:
+                    maxi = max(maxi, a[i][j1] + self.totalFromCurrPointOnwards(a, i+1, jj1, jj2, dp))
+                else:
+                    maxi = max(maxi, a[i][j1] + a[i][j2] + self.totalFromCurrPointOnwards(a, i+1, jj1, jj2, dp))
+        
+        dp[i][j1][j2] = maxi
 
-        return dp[i][j1][j2]
+        return maxi
 
     def cherryPickup(self, a: List[List[int]]) -> int:
         m, n = len(a), len(a[0])
-        dp = [[-1]*n for _ in range(n)]
-        
-        for j1 in range(n):
-            for j2 in range(n):
-                dp[j1][j2] = a[m-1][j1] if j1==j2 else (a[m-1][j1] + a[m-1][j2])
+        dp = [[[-1]*n for _ in range(n)] for _ in range(m)]
 
-        for i in range(m-2, -1, -1):
-            temp = [[-1]*n for _ in range(n)]
-            for j1 in range(n):
-                for j2 in range(n):
-                    curr_state_gain = a[i][j1] + a[i][j2] if j1!=j2 else a[i][j1]
-                    nxt_state_max_gain = float('-inf')
-                    for dj1 in range(-1,2,1):
-                        for dj2 in range(-1,2,1):
-                            if (j1+dj1<0 or j1+dj1>=n) or (j2+dj2<0 or j2+dj2>=n):
-                                continue
-                            nxt_state_max_gain = max(nxt_state_max_gain, dp[j1+dj1][j2+dj2])
-                    temp[j1][j2] = curr_state_gain + nxt_state_max_gain
-            dp = temp                
-
-        return dp[0][n-1]    
+        return self.totalFromCurrPointOnwards(a, 0, 0, n-1, dp)
