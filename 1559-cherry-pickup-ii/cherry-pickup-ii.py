15d48c5b90d092c40d04193c00c5c0edd21971ed
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-12 17:53:00 -0500

    Time: 1043 ms (44.32%) | Memory: 26.6 MB (70.40%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -1,5 +1,5 @@
 class Solution:
-    def totalFromCurrPointOnwards(self, a, i, j1, j2, dp):
+    def totalFromCurrStateOnwards(self, a, i, j1, j2, dp):
         m, n = len(a), len(a[0])
 
         if (j1 < 0 or j1 >= n) or (j2 < 0 or j2 >= n):
@@ -21,9 +21,9 @@ class Solution:
             for dj2 in range(-1, 2):
                 jj1, jj2 = j1+dj1, j2+dj2
                 if j1 == j2:
-                    maxi = max(maxi, a[i][j1] + self.totalFromCurrPointOnwards(a, i+1, jj1, jj2, dp))
+                    maxi = max(maxi, a[i][j1] + self.totalFromCurrStateOnwards(a, i+1, jj1, jj2, dp))
                 else:
-                    maxi = max(maxi, a[i][j1] + a[i][j2] + self.totalFromCurrPointOnwards(a, i+1, jj1, jj2, dp))
+                    maxi = max(maxi, a[i][j1] + a[i][j2] + self.totalFromCurrStateOnwards(a, i+1, jj1, jj2, dp))
         
         dp[i][j1][j2] = maxi
 
@@ -33,4 +33,4 @@ class Solution:
         m, n = len(a), len(a[0])
         dp = [[[-1]*n for _ in range(n)] for _ in range(m)]
 
-        return self.totalFromCurrPointOnwards(a, 0, 0, n-1, dp)
+        return self.totalFromCurrStateOnwards(a, 0, 0, n-1, dp)
