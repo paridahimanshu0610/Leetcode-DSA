055dd73142a8433530d2005ce483f480195bd671
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-25 18:31:50 -0500

    Time: 1083 ms (40.86%) | Memory: 19.8 MB (84.40%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -32,12 +32,30 @@ class Solution:
     def cherryPickup(self, a: List[List[int]]) -> int:
         m, n = len(a), len(a[0])
 
-        dp = [[[None]*n for _ in range(n)] for _ in range(m)]
+        dp = [[-float('inf')]*n for _ in range(n)]
+        dp[0][n-1] = a[0][0] + a[0][n-1]
+
+        for r in range(1, m):
+            temp = [[None]*n for _ in range(n)]
+
+            for c1 in range(n):
+                for c2 in range(n):
+
+                    maxi = -float('inf')
+                    for j1 in range(c1-1, c1+2):
+                        for j2 in range(c2-1, c2+2):
+                            if (j1 < 0 or j1 >= n) or (j2 < 0 or j2 >= n):
+                                continue
+                            maxi = max(maxi, dp[j1][j2])
+                    
+                    maxi = a[r][c1] + maxi if c1 == c2 else a[r][c1] + a[r][c2] + maxi
+                    temp[c1][c2] = maxi
+
+            dp = temp                  
 
         maxi = -float('inf')
-
-        for j1 in range(n):
-            for j2 in range(n):
-                maxi = max(maxi, self.soFar(a, m-1, j1, j2, dp))
-
+        for c1 in range(n):
+            for c2 in range(n):
+                maxi = max(maxi, dp[c1][c2])
+                
         return maxi
