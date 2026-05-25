4fc1a40a400e8ac2d5afa3f0b12c8d8a2c05f672
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-25 18:15:58 -0500

    Time: 2458 ms (5.12%) | Memory: 29.4 MB (47.80%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -1,36 +1,43 @@
 class Solution:
-    def totalFromCurrStateOnwards(self, a, i, j1, j2, dp):
-        m, n = len(a), len(a[0])
+    def soFar(self, a, r, c1, c2, dp):
+        n = len(a[0])
 
-        if (j1 < 0 or j1 >= n) or (j2 < 0 or j2 >= n):
+        if r < 0 or (c1 < 0 or c1 >= n) or (c2 < 0 or c2 >= n):
             return -float('inf')
-
-        if i == m-1:
-            if j1 == j2:
-                dp[i][j1][j2] = a[i][j1]
-            else:
-                dp[i][j1][j2] = a[i][j1] + a[i][j2]
-
-            return dp[i][j1][j2]
-
-        if dp[i][j1][j2] != -1:
-            return dp[i][j1][j2]
-
-        maxi = 0
-        for dj1 in range(-1, 2):
-            for dj2 in range(-1, 2):
-                jj1, jj2 = j1+dj1, j2+dj2
-                if j1 == j2:
-                    maxi = max(maxi, a[i][j1] + self.totalFromCurrStateOnwards(a, i+1, jj1, jj2, dp))
-                else:
-                    maxi = max(maxi, a[i][j1] + a[i][j2] + self.totalFromCurrStateOnwards(a, i+1, jj1, jj2, dp))
         
-        dp[i][j1][j2] = maxi
+        if r == 0:
+            if c1 == 0 and c2 == n-1:
+                dp[0][c1][c2] = a[0][c1] + a[0][c2]
+            else:
+                dp[0][c1][c2] = -float('inf')
+            
+            return dp[0][c1][c2]
+
+        if dp[r][c1][c2] is not None:
+            return dp[r][c1][c2]
+
+        maxi = -float('inf')
+
+        for j1 in range(c1-1, c1+2):
+            for j2 in range(c2-1, c2+2):
+                if (j1 < 0 or j1 >= n) or (j2 < 0 or j2 >= n):
+                    continue
+                maxi = max(maxi, self.soFar(a, r-1, j1, j2, dp))
+        
+        maxi = a[r][c1] + maxi if c1 == c2 else a[r][c1] + a[r][c2] + maxi
+        dp[r][c1][c2] = maxi
 
         return maxi
 
     def cherryPickup(self, a: List[List[int]]) -> int:
         m, n = len(a), len(a[0])
-        dp = [[[-1]*n for _ in range(n)] for _ in range(m)]
 
-        return self.totalFromCurrStateOnwards(a, 0, 0, n-1, dp)
+        dp = [[[None]*n for _ in range(n)] for _ in range(m)]
+
+        maxi = -float('inf')
+
+        for j1 in range(n):
+            for j2 in range(n):
+                maxi = max(maxi, self.soFar(a, m-1, j1, j2, dp))
+
+        return maxi
