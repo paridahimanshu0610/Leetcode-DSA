83a19356fa37f2b0fb9b8ee47c41679f889a8000
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-25 19:12:59 -0500

    Time: 2 ms (93.17%) | Memory: 20.1 MB (98.94%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,36 +3,29 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        x, y = None, None
+        i0, j0 = -1, -1
+        m, n = len(a), len(a[0])
 
-        for i in range(len(a)):
-            for j in range(len(a[0])):
-                if a[i][j]==0:  
-                    if x is None:
-                        x, y = i, j
+        for i in range(m):
+            for j in range(n):
+                if a[i][j] == 0:
+                    if i0 == -1:
+                        i0, j0 = i, j
                     else:
-                        a[x][j], a[i][y] = 0, 0
+                        a[i0][j], a[i][j0] = 0, 0
         
-        if x is None:
+        if i0 == -1:
             return
 
-        for i in range(len(a)):
-            if i==x:
-                continue
-            if a[i][y]==0:
-                for j in range(len(a[0])):
+        for i in range(m):
+            if a[i][j0] == 0 and i != i0:
+                for j in range(n):
                     a[i][j] = 0
 
-        for j in range(len(a[0])):
-            # No longer required
-            # if j==y:
-            #     continue
-            if a[x][j]==0:
-                for i in range(len(a)):
+        for j in range(n):
+            if a[i0][j] == 0:
+                for i in range(m):
                     a[i][j] = 0
         
-        for i in range(len(a)):
-            a[i][y] = 0
-        
-        for j in range(len(a[0])):
-            a[x][j] = 0
+        for j in range(n):
+            a[i0][j] = 0
