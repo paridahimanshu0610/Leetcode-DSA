2cd9c5b1355db28e1790e31888c2d34a9426ae04
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-27 16:08:16 -0500

    Time: 1177 ms (35.69%) | Memory: 19.4 MB (71.12%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -26,6 +26,23 @@ class Solution:
         target = target // 2
         n = len(a)
 
-        dp = [[None]*(target+1) for _ in range(n)]
+        dp = [False]*(target+1)
 
-        return self.soFar(a, n-1, target, dp)
+        if a[0] <= target:
+            dp[a[0]] = True
+
+        for idx in range(1, n):
+            temp = [None]*(target+1)
+            for t1 in range(target+1):
+                take = False
+                # Take condition
+                if a[idx] <= t1:
+                    take = dp[t1-a[idx]]
+                
+                notTake = dp[t1]
+
+                temp[t1] = (take or notTake)
+
+            dp = temp
+
+        return dp[target]
