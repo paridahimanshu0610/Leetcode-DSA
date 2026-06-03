ae66559532f1fb785364d3d35dbbc951cb749e02
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-03 16:16:02 -0500

    Time: 299 ms (85.36%) | Memory: 44.4 MB (45.93%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -21,6 +21,35 @@ class Solution:
 
     def longestCommonSubsequence(self, s1: str, s2: str) -> int:
         n, m = len(s1), len(s2)
-        dp = [[None]*m for _ in range(n)]
+        dp = [[0]*m for _ in range(n)]
 
-        return self.soFar(s1, s2, n-1, m-1, dp)
+        idx2 = 0
+        while idx2 < m:
+            if s1[0] == s2[idx2]:
+                break
+            idx2 += 1
+
+        while idx2 < m:
+            dp[0][idx2] = 1
+            idx2 += 1
+
+        idx1 = 0
+        while idx1 < n:
+            if s2[0] == s1[idx1]:
+                break
+            idx1 += 1
+
+        while idx1 < n:
+            dp[idx1][0] = 1
+            idx1 += 1
+
+        for idx1 in range(1, n):
+            for idx2 in range(1, m):
+                if s1[idx1] == s2[idx2]:
+                    res = 1 + dp[idx1-1][idx2-1]
+                else:
+                    res = max(dp[idx1][idx2-1], dp[idx1-1][idx2])
+
+                dp[idx1][idx2] = res
+
+        return dp[n-1][m-1]
