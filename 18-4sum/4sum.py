7e707338554f6a207b5d84b598fc6e53e0bbe039
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-03 19:49:06 -0500

    Time: 1051 ms (5.01%) | Memory: 19.3 MB (54.85%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,44 +1,15 @@
 class Solution:
     def fourSum(self, a: List[int], target: int) -> List[List[int]]:
-        if len(a) < 4:
-            return []
-        
-        a.sort()
+        res = set()
         n = len(a)
-        res = []
-        i = 0
 
-        while i < n-3:
-            if i > 0 and a[i] == a[i-1]:
-                i += 1
-                continue
-            j = i+1
-            while j < n-2:
-                if j > i+1 and a[j] == a[j-1]:
-                    j += 1
-                    continue
-                
-                p, q = j+1, n-1
-                
-                while p < q and p < n-1 and q < n:
-                    currSum = a[i] + a[j] + a[p] + a[q]
+        for i in range(n-3):
+            for j in range(i+1, n-2):
+                visited = set()
+                for k in range(j+1, n):
+                    required = target - (a[i]+a[j]+a[k])
+                    if required in visited:
+                        res.add(tuple(sorted([a[i], a[j], a[k], required])))
+                    visited.add(a[k])
 
-                    if currSum > target:
-                        q -= 1
-                    elif currSum < target:
-                        p += 1
-                    else:
-                        res.append([a[i], a[j], a[p], a[q]])
-                        currLeft, currRight = a[p], a[q]
-
-                        while a[p] == currLeft and p < q:
-                            p += 1
-                        
-                        while a[q] == currRight and p < q:
-                            q -= 1
-                
-                j += 1
-            
-            i += 1
-
-        return res
+        return [list(temp) for temp in res]
