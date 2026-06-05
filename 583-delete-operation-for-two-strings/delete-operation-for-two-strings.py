a25ef767818696611fe93aa86ee3df288fdcf383
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-05 15:21:29 -0500

    Time: 95 ms (30.22%) | Memory: 21.7 MB (42.45%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def soFar(self, s1, s2, idx1, idx2, dp):
+        if idx1 < 0 or idx2 < 0:
+            return 0
+
+        if dp[idx1][idx2] is not None:
+            return dp[idx1][idx2]
+
+        if s1[idx1] == s2[idx2]:
+            dp[idx1][idx2] = 1 + self.soFar(s1, s2, idx1-1, idx2-1, dp)
+        else:
+            dp[idx1][idx2] = max(self.soFar(s1, s2, idx1, idx2-1, dp), self.soFar(s1, s2, idx1-1, idx2, dp))
+
+        return dp[idx1][idx2]
+        
+    def minDistance(self, s1: str, s2: str) -> int:
+        m, n = len(s1), len(s2)
+        dp = [[None]*n for _ in range(m)]
+
+        self.soFar(s1, s2, m-1, n-1, dp)
+
+        return (n-dp[m-1][n-1]) + (m-dp[m-1][n-1])
