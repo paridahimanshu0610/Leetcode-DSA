f8178f3f02f47919fcfb1c0e48b078bd81c91ea6
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-05 15:09:23 -0500

    Time: 812 ms (9.67%) | Memory: 21.3 MB (50.22%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,32 +1,20 @@
 class Solution:
     def soFar(self, s, idx1, idx2, dp):
-        if idx2 >= idx1:
-            dp[idx1][idx2] = 0
-            return dp[idx1][idx2]
+        if (idx2 >= len(s)) or (idx1 < 0):
+            return 0
         
         if dp[idx1][idx2] is not None:
             return dp[idx1][idx2]
 
         if s[idx1] == s[idx2]:
-            dp[idx1][idx2] = self.soFar(s, idx1-1, idx2+1, dp)
+            dp[idx1][idx2] = 1 + self.soFar(s, idx1-1, idx2+1, dp)
         else:
-            dp[idx1][idx2] = 1 + min(self.soFar(s, idx1, idx2+1, dp), self.soFar(s, idx1-1, idx2, dp))
+            dp[idx1][idx2] = max(self.soFar(s, idx1, idx2+1, dp), self.soFar(s, idx1-1, idx2, dp))
 
         return dp[idx1][idx2]
 
     def minInsertions(self, s: str) -> int:
         n = len(s)
-        dp = [0]*n
+        dp = [[None]*n for _ in range(n)]
 
-        for idx1 in range(1, n):
-            temp = [0]*n
-            for idx2 in range(n-1, -1, -1):
-                if idx2 >= idx1:
-                    temp[idx2] = 0
-                elif s[idx1] == s[idx2]:
-                    temp[idx2] = dp[idx2+1]
-                else:
-                    temp[idx2] = 1 + min(temp[idx2+1], dp[idx2])
-            dp = temp
-
-        return dp[0]
+        return n - self.soFar(s, n-1, 0, dp)
