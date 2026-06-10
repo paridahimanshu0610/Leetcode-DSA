eb269b40f030e8a4567a84fba02a1ab35e45a539
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-10 00:22:26 -0500

    Time: 589 ms (45.14%) | Memory: 19.6 MB (57.75%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.py
@@ -0,0 +1,58 @@
+class Solution:
+    def soFar(self, s, p, idx1, idx2, dp):
+        if idx1 < 0:
+            if idx2 < 0:
+                return True
+            else:
+                for i in range(idx2+1):
+                    if p[idx2] != "*":
+                        return False
+                return True
+        elif idx2 < 0:
+            return idx1 < 0
+
+        if dp[idx1][idx2] is not None:
+            return dp[idx1][idx2]
+
+        if s[idx1] == p[idx2]:
+            dp[idx1][idx2] = True and self.soFar(s, p, idx1-1, idx2-1, dp)
+        else:
+            if p[idx2] == "*":
+                dp[idx1][idx2] = self.soFar(s, p, idx1-1, idx2, dp) or self.soFar(s, p, idx1-1, idx2-1, dp) or self.soFar(s, p, idx1, idx2-1, dp) 
+            elif p[idx2] == "?":
+                dp[idx1][idx2] = self.soFar(s, p, idx1-1, idx2-1, dp)
+            else:
+                dp[idx1][idx2] = False
+
+        return dp[idx1][idx2]
+
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        # dp = [[None]*n for _ in range(m)]
+
+        dp = [False]*(n+1)
+        dp[0] = True
+
+        i = 1
+        while i < n+1:
+            if p[i-1] == "*":
+                dp[i] = True
+            else:
+                break
+            i += 1
+        
+        for idx1 in range(1, m+1):
+            temp = [False]*(n+1)
+            for idx2 in range(1, n+1):
+                if s[idx1-1] == p[idx2-1]:
+                    temp[idx2] = dp[idx2-1]
+                else:
+                    if p[idx2-1] == "*":
+                        temp[idx2] = dp[idx2] or dp[idx2-1] or temp[idx2-1]
+                    elif p[idx2-1] == "?":
+                        temp[idx2] = dp[idx2-1]
+                    else:
+                        temp[idx2] = False
+            dp = temp
+
+        return dp[n]
