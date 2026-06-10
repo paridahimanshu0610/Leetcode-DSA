d7766282207bd40def03fbfe2817524d108249a1
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-09 22:53:18 -0500

    Time: 16 ms (99.41%) | Memory: 21.2 MB (87.96%) - LeetSync

---
+++ 72-edit-distance/edit-distance.py
@@ -0,0 +1,24 @@
+class Solution:
+    def soFar(self, s1, s2, idx1, idx2, dp):
+        if idx1 < 0 and idx2 >= 0:
+            return idx2+1
+        elif idx2 < 0 and idx1 >= 0:
+            return idx1+1
+        elif idx1 < 0 and idx2 < 0:
+            return 0
+
+        if dp[idx1][idx2] is not None:
+            return dp[idx1][idx2]
+
+        if s1[idx1] == s2[idx2]:
+            dp[idx1][idx2] = self.soFar(s1, s2, idx1-1, idx2-1, dp)
+        else:
+            dp[idx1][idx2] = 1 + min(self.soFar(s1, s2, idx1-1, idx2-1, dp), self.soFar(s1, s2, idx1-1, idx2, dp), self.soFar(s1, s2, idx1, idx2-1, dp))
+        
+        return dp[idx1][idx2]
+
+    def minDistance(self, s1: str, s2: str) -> int:
+        m, n = len(s1),len(s2)
+        dp = [[None]*n for _ in range(m)]
+
+        return self.soFar(s1, s2, m-1, n-1, dp)
