c297e3ef5ee43c769cf69843acb7c3207a0d48b1
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-11 09:58:01 -0500

    Time: 255 ms (93.71%) | Memory: 19.3 MB (92.51%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -5,7 +5,7 @@ class Solution:
 
         if dp[idx1][idx2] is not None:
             return dp[idx1][idx2]
-            
+
         if s1[idx1] == s2[idx2]:
             dp[idx1][idx2] = 1 + self.soFar(s1, s2, idx1-1, idx2-1, dp)
         else:
@@ -15,6 +15,15 @@ class Solution:
 
     def longestCommonSubsequence(self, s1: str, s2: str) -> int:
         m, n = len(s1), len(s2)
-        dp = [[None]*n for _ in range(m)]
+        dp = [0]*(n+1)
+        
+        for idx1 in range(1, m+1):
+            temp = [0]*(n+1)
+            for idx2 in range(1, n+1):
+                if s1[idx1-1] == s2[idx2-1]:
+                    temp[idx2] = 1 + dp[idx2-1]
+                else:
+                    temp[idx2] = max(temp[idx2-1], dp[idx2])
+            dp = temp
 
-        return self.soFar(s1, s2, m-1, n-1, dp)
+        return dp[n]
