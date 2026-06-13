8afdff169a1d9e49f7acb906166fac0704d6769d
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-12 21:34:44 -0500

    Time: 419 ms (32.30%) | Memory: 54.6 MB (22.27%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.py
@@ -1,53 +1,38 @@
 class Solution:
-    def soFar(self, s1,  s2, idx1, idx2, dp):
-        if idx1 < 0 or idx2 < 0:
-            return 0
-        
-        if dp[idx1][idx2] is not None:
-            return dp[idx1][idx2]
-
-        if s1[idx1] == s2[idx2]:
-            dp[idx1][idx2] = 1 + self.soFar(s1, s2, idx1-1, idx2-1, dp)
-        else:
-            dp[idx1][idx2] = max(self.soFar(s1, s2, idx1, idx2-1, dp), self.soFar(s1, s2, idx1-1, idx2, dp))
-
-        return dp[idx1][idx2]
-
     def shortestCommonSupersequence(self, s1: str, s2: str) -> str:
         m, n = len(s1), len(s2)
-        dp = [[None]*n for _ in range(m)]
+        dp = [[0]*(n+1) for _ in range(m+1)]
 
-        self.soFar(s1, s2, m-1, n-1, dp) 
-
-        maxLen = m+n-dp[m-1][n-1]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         
-        i, j = m-1, n-1
+        print(dp)
+        i, j = m, n
         res = []
 
-        while i >= 0 and j >= 0:
-            if s1[i] == s2[j]:
-                res.append(s1[i])
+        while i >= 1 and j >= 1:
+            if s1[i-1] == s2[j-1]:
+                res.append(s1[i-1])
                 i -= 1
                 j -= 1
-            elif i == 0:
-                res.append(s2[j])
-                j -= 1
-            elif j == 0:
-                res.append(s1[i])
-                i -= 1
-            elif (dp[i-1][j] >= dp[i][j-1]):
-                res.append(s1[i])
-                i -= 1
             else:
-                res.append(s2[j])
-                j -= 1
+                if dp[i][j-1] >= dp[i-1][j]:
+                    res.append(s2[j-1])
+                    j -= 1
+                else:
+                    res.append(s1[i-1])
+                    i -= 1
 
-        while i >= 0:
-            res.append(s1[i])
+        while i >= 1:
+            res.append(s1[i-1])
             i -= 1
-
-        while j >= 0:
-            res.append(s2[j])
-            j -= 1 
+        
+        while j >= 1:
+            res.append(s2[j-1])
+            j -= 1
 
         return "".join(res[::-1])
