349fba2a2507aeaa10eb5018b814793eba103d76
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-13 00:42:59 -0500

    Time: 613 ms (43.84%) | Memory: 19.4 MB (71.36%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -5,20 +5,20 @@ class Solution:
                 return True
             else:
                 for i in range(idx2+1):
-                    if p[idx2] != "*":
+                    if p[i] != "*":
                         return False
                 return True
         elif idx2 < 0:
             return idx1 < 0
-
+        
         if dp[idx1][idx2] is not None:
             return dp[idx1][idx2]
 
         if s[idx1] == p[idx2]:
-            dp[idx1][idx2] = True and self.soFar(s, p, idx1-1, idx2-1, dp)
+            dp[idx1][idx2] = self.soFar(s, p, idx1-1, idx2-1, dp)
         else:
             if p[idx2] == "*":
-                dp[idx1][idx2] = self.soFar(s, p, idx1-1, idx2, dp) or self.soFar(s, p, idx1-1, idx2-1, dp) or self.soFar(s, p, idx1, idx2-1, dp) 
+                dp[idx1][idx2] = self.soFar(s, p, idx1-1, idx2-1, dp) or self.soFar(s, p, idx1, idx2-1, dp) or self.soFar(s, p, idx1-1, idx2, dp)
             elif p[idx2] == "?":
                 dp[idx1][idx2] = self.soFar(s, p, idx1-1, idx2-1, dp)
             else:
@@ -29,18 +29,15 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
         # dp = [[None]*n for _ in range(m)]
-
         dp = [False]*(n+1)
         dp[0] = True
 
-        i = 1
-        while i < n+1:
+        for i in range(1, n+1):
             if p[i-1] == "*":
                 dp[i] = True
             else:
                 break
-            i += 1
-        
+
         for idx1 in range(1, m+1):
             temp = [False]*(n+1)
             for idx2 in range(1, n+1):
@@ -48,11 +45,12 @@ class Solution:
                     temp[idx2] = dp[idx2-1]
                 else:
                     if p[idx2-1] == "*":
-                        temp[idx2] = dp[idx2] or dp[idx2-1] or temp[idx2-1]
+                        temp[idx2] = dp[idx2-1] or dp[idx2] or temp[idx2-1]
                     elif p[idx2-1] == "?":
                         temp[idx2] = dp[idx2-1]
                     else:
                         temp[idx2] = False
+
             dp = temp
 
         return dp[n]
