d388f9b60a39f27469ae9282fabdf724801fafb7
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-12 23:54:25 -0500

    Time: 262 ms (74.75%) | Memory: 19.6 MB (82.42%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,19 +1,5 @@
 class Solution:
     def soFar(self, s, t, idx1, idx2, dp):
-        if idx1 < 0 or idx2 < 0:
-            return 0
-
-        if dp[idx1][idx2] is not None:
-            return dp[idx1][idx2]
-
-        if s[idx1] == t[idx2]:
-            dp[idx1][idx2] = 1 + self.soFar(s, t, idx1-1, idx2-1, dp)
-        else:
-            dp[idx1][idx2] = max(self.soFar(s, t, idx1-1, idx2, dp), self.soFar(s, t, idx1, idx2-1, dp))
-
-        return dp[idx1][idx2]
-    
-    def getAllLCS(self, s, t, idx1, idx2, dp):
         if idx2 < 0:
             return 1
         elif idx1 < 0:
@@ -23,23 +9,26 @@ class Solution:
             return dp[idx1][idx2]
 
         if s[idx1] == t[idx2]:
-            dp[idx1][idx2] = self.getAllLCS(s, t, idx1-1, idx2-1, dp) + self.getAllLCS(s, t, idx1-1, idx2, dp)
+            dp[idx1][idx2] = self.soFar(s, t, idx1-1, idx2-1, dp) + self.soFar(s, t, idx1-1, idx2, dp)
         else:
-            dp[idx1][idx2] = self.getAllLCS(s, t, idx1-1, idx2, dp)
+            dp[idx1][idx2] = self.soFar(s, t, idx1-1, idx2, dp)
 
         return dp[idx1][idx2]
 
     def numDistinct(self, s: str, t: str) -> int:
         m, n = len(s), len(t)
         # dp = [[None]*n for _ in range(m)]
+        dp = [0]*(n+1)
+        dp[0] = 1
+        
+        for i in range(1, m+1):
+            temp = [0]*(n+1)
+            temp[0] = 1
+            for j in range(1, n+1):
+                if s[i-1] == t[j-1]:
+                    temp[j] = dp[j-1] + dp[j]
+                else:
+                    temp[j] = dp[j]
+            dp = temp 
 
-        # self.soFar(s, t, m-1, n-1, dp)
-        # maxLen = dp[m-1][n-1]
-
-        # if maxLen < n:
-        #     return 0
-
-        dp = [[None]*n for _ in range(m)]
-        self.getAllLCS(s, t, m-1, n-1, dp)
-
-        return dp[m-1][n-1]
+        return dp[n]
