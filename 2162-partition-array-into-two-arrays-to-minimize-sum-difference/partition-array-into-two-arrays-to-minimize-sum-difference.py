1499bd54d9ea3f5ef830d57a1c536f1ff6d9a7f3
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-13 18:09:37 -0500

    Time: 1751 ms (69.15%) | Memory: 22.6 MB (27.66%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.py
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.py
@@ -1,37 +1,38 @@
 from itertools import combinations
 import bisect
 
-
 class Solution:
+    def getCombinations(self, a):
+        n = len(a)
+        res = {}
+
+        for k in range(1, n+1):
+            all_sum_set = set()
+            for comb in combinations(a, k):
+                all_sum_set.add(sum(comb))
+            res[k] = list(all_sum_set)
+
+        return res
+
     def minimumDifference(self, a: List[int]) -> int:
-        def get_sums(arr):
-            N = len(arr)
-            ans = {}
-            for k in range(1, N + 1):
-                all_sums = set()
-                for comb in combinations(arr, k):
-                    all_sums.add(sum(comb))
-                ans[k] = list(all_sums)
-
-            return ans
-
         n = len(a) // 2
-        lsums, rsums = get_sums(a[:n]), get_sums(a[n:])
         total = sum(a)
-        half = total // 2
-        minDiff = abs(sum(a[:n]) - sum(a[n:]))
+        target = total // 2
+
+        left_sums, right_sums = self.getCombinations(a[:n]), self.getCombinations(a[n:])
+        minDiff = abs(sum(a[:n])-sum(a[n:]))
 
         for k in range(1, n):
-            curr_right_sums = rsums[n - k]
+            curr_right_sums = right_sums[n-k]
             curr_right_sums.sort()
-            for left_sum in lsums[k]:
-                right_target_sum = half - left_sum
-                # p is the index of element just greater than or equal to right_target_sum
-                p = bisect.bisect_left(curr_right_sums, right_target_sum)
-                for q in [p, p - 1]:
+            for curr_left_sum in left_sums[k]:
+                right_sum_target = target - curr_left_sum 
+                # Retrieving the position index of the element just greater than right_sum_target
+                p = bisect.bisect_left(curr_right_sums, right_sum_target)
+                for q in [p-1, p]:
                     if 0 <= q < len(curr_right_sums):
-                        group1 = left_sum + curr_right_sums[q]
-                        group2 = total - group1
-                        minDiff = min(abs(group1 - group2), minDiff)
+                        curr_right_sum = curr_right_sums[q]
+                        curr_total = curr_left_sum + curr_right_sum
+                        minDiff = min(minDiff, abs(curr_total - (total-curr_total)))
 
         return minDiff
