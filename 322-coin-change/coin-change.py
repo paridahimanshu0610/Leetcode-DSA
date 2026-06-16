e6a90c3c1e5a1ee48fb36349b85441ba57dcbc4f
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-16 17:48:37 -0500

    Time: 1503 ms (5.00%) | Memory: 21.9 MB (26.18%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,68 +1,41 @@
 class Solution:
+    def compare(self, n1, n2):
+        if n1 >= 0 and n2 >= 0:
+            return min(n1, n2)
+        elif n1 >= 0:
+            return n1
+        elif n2 >= 0:
+            return n2
+        else:
+            return -1
+
     def soFar(self, a, idx, k, dp):
-        if k == 0:
+        if idx < 0:
+            return -1
+        elif k == 0:
             dp[idx][k] = 0
-            return dp[idx][k]
-        
-        if idx == 0:
-            if k % a[idx] == 0:
-                dp[idx][k] = k // a[idx]
-            else:
-                dp[idx][k] = -1
-            return dp[idx][k]
-        
+            return 0
+
         if dp[idx][k] is not None:
             return dp[idx][k]
-
+        
         notTake = self.soFar(a, idx-1, k, dp)
-
         take = -1
         if a[idx] <= k:
-            temp = self.soFar(a, idx, k-a[idx], dp)
-            if temp >= 0: 
-                take = 1 + temp
+            temp1, temp2 = self.soFar(a, idx, k - a[idx], dp), self.soFar(a, idx-1, k - a[idx], dp)
+            temp = self.compare(temp1, temp2)
+            take = (1 + temp) if temp >= 0 else -1
         
-        if take >=0 and notTake >= 0:
-            dp[idx][k] = min(take, notTake)
-        elif take >= 0:
-            dp[idx][k] = take
-        elif notTake >= 0:
-            dp[idx][k] = notTake
-        else:
-            dp[idx][k] = -1
-
+        dp[idx][k] = self.compare(take, notTake)
+ 
         return dp[idx][k]
 
-    def coinChange(self, a: List[int], target: int) -> int:
+    def coinChange(self, a: List[int], amount: int) -> int:
         n = len(a)
-        dp = [-1]*(target+1)
-        dp[0] = 0
+        dp = [[None]*(amount+1) for _ in range(n)]
 
-        if a[0] <= target:
-            for k in range(1, target+1):
-                if k % a[0] == 0:
-                    dp[k] = k // a[0]
+        self.soFar(a, n-1, amount, dp)
 
-        for idx in range(1, n):
-            temp = [-1]*(target+1)
-            temp[0] = 0
-            for k in range(1, target+1):
-                notTake = dp[k]
+        res = dp[n-1][amount]
 
-                take = -1
-                if a[idx] <= k:
-                    temp0 = temp[k-a[idx]]
-                    if temp0 >= 0: 
-                        take = 1 + temp0
-                
-                if take >=0 and notTake >= 0:
-                    temp[k] = min(take, notTake)
-                elif take >= 0:
-                    temp[k] = take
-                elif notTake >= 0:
-                    temp[k] = notTake
-                else:
-                    temp[k] = -1
-            dp = temp
-
-        return dp[target] 
+        return res
