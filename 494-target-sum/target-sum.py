b26737234069cb37688b34babb70e7e7fda52683
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-16 18:11:28 -0500

    Time: 135 ms (51.90%) | Memory: 19.6 MB (49.88%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,51 +1,23 @@
 class Solution:
     def soFar(self, a, idx, k, dp):
         if idx == 0:
-            if (k == a[idx]) or (k == -a[idx]):
-                if k != 0:
-                    dp[(idx, k)] = 1
-                else:
-                    dp[(idx, k)] = 2
+            if k == 0:
+                return 2 if a[idx] == 0 else 0
             else:
-                dp[(idx, k)] = 0
+                return 1 if (a[idx] == k) or (a[idx] == -k) else 0
 
-            return dp[(idx, k)]
-        
-        if (idx, k) in dp:
-            return dp[(idx, k)]
-        
-        plus = self.soFar(a, idx-1, k-a[idx], dp)
-        minus = self.soFar(a, idx-1, k+a[idx], dp)
+        if (idx,k) in dp:
+            return dp[(idx,k)]
 
-        dp[(idx, k)] = plus + minus
+        add = self.soFar(a, idx-1, k - a[idx], dp)
+        subtract = self.soFar(a, idx-1, k + a[idx], dp)
 
-        return dp[(idx, k)]
+        dp[(idx,k)] = add + subtract
+
+        return dp[(idx,k)]
 
     def findTargetSumWays(self, a: List[int], target: int) -> int:
         n = len(a)
-        total = sum(a)
-        lower, upper = target - total, target + total
-        dp = [0]*(2*total+1)
+        dp = {}
 
-        for k in range(lower, upper+1):
-            if (k == a[0]) or (k == -a[0]):
-                if k != 0:
-                    dp[k-lower] = 1
-                else:
-                    dp[k-lower] = 2
-            else:
-                dp[k-lower] = 0
-
-        for idx in range(1, n):
-            temp = [0]*(2*total+1)
-            for k in range(lower, upper+1):
-                plus, minus = 0, 0
-                if 0 <= k-a[idx]-lower <= 2*total:
-                    plus = dp[k-a[idx]-lower]
-                if 0 <= k+a[idx]-lower <= 2*total:  
-                    minus = dp[k+a[idx]-lower]
-                temp[k-lower] = plus + minus
-
-            dp = temp
-
-        return dp[target-lower]
+        return self.soFar(a, n-1, target, dp)
