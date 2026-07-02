0b2f43cb5d8c2fbdea3b857fa811afdb85bae2db
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-01 21:29:52 -0500

    Time: 379 ms (62.01%) | Memory: 19.6 MB (66.45%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -22,6 +22,24 @@ class Solution:
     def minCost(self, n: int, a: List[int]) -> int:
         a.sort()
         m = len(a)
-        dp = [[None] * m for _ in range(m)]
+        dp = [[0] * m for _ in range(m)]
 
-        return self.cutCost(a, 0, m-1, n, dp)
+        for currItems in range(1, m+1):
+            for l in range(0, m-currItems+1):
+                h = l+currItems-1
+                mini = float('inf')
+
+                leftLastCut = a[l-1] if l-1 >= 0 else 0
+                rightLastCut = a[h+1] if h+1 < len(a) else n
+                currLen = rightLastCut - leftLastCut 
+
+                for i in range(l, h+1):
+                    leftPart = dp[l][i-1] if i-1 >= 0 else 0
+                    rightPart = dp[i+1][h] if i+1 < m else 0
+
+                    currCost = currLen + leftPart + rightPart
+                    mini = min(mini, currCost)
+                
+                dp[l][h]  = mini
+
+        return dp[0][m-1]
