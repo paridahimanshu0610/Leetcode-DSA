9ae83aecf8f7a1e51ec4653cf973cafd77e0629a
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-05 17:56:14 -0500

    Time: 240 ms (5.53%) | Memory: 33.7 MB (30.14%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -0,0 +1,51 @@
+class Solution:
+    def nextSmallerEle(self, a):
+        n = len(a)
+        res = [None] * n
+        stack = []
+
+        for i in range(n):
+            if (len(stack) == 0) or (a[i] >= a[stack[-1]]):
+                stack.append(i)
+            else:
+                while len(stack) > 0 and a[stack[-1]] > a[i]:
+                    res[stack[-1]] = i  # a[i]
+                    stack.pop()
+                stack.append(i)
+
+        while len(stack) > 0:
+            res[stack[-1]] = n
+            stack.pop()
+
+        return res
+
+    def prevSmallerEle(self, a):
+        n = len(a)
+        res = [None] * n
+        stack = []
+
+        for i in range(n - 1, -1, -1):
+            if len(stack) == 0 or a[i] >= a[stack[-1]]:
+                stack.append(i)
+            else:
+                while len(stack) != 0 and a[stack[-1]] > a[i]:
+                    res[stack[-1]] = i  # a[i]
+                    stack.pop()
+                stack.append(i)
+
+        while len(stack) > 0:
+            res[stack[-1]] = -1
+            stack.pop()
+
+        return res
+
+    def largestRectangleArea(self, a: List[int]) -> int:
+        pse, nse = self.prevSmallerEle(a), self.nextSmallerEle(a)
+
+        n = len(a)
+        res = -float("inf")
+
+        for i in range(n):
+            res = max(res, (nse[i] - pse[i] - 1) * a[i])
+
+        return res
