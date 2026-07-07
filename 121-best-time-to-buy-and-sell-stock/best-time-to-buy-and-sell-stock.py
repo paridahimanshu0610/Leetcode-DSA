74a9c65737f2abdc4fe803eb9c69e511bbef4285
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-06 19:22:19 -0500

    Time: 49 ms (55.41%) | Memory: 28.6 MB (74.74%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxProfit(self, a: List[int]) -> int:
-        curr_holding = a[0]
-        res = 0
+        n, curr, res = len(a), a[0], 0
 
-        for i in range(1, len(a)):
-            if a[i] < curr_holding:
-                curr_holding = a[i]
-            
-            res = max(res, a[i] - curr_holding)
+        for i in range(1, n):
+            if a[i] <= curr:
+                curr = a[i]
+            else:
+                res = max(res, a[i]-curr)
 
         return res
