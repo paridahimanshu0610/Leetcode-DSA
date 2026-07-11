a99a2d5f88fe1030e36689fee9bfa48b179c6de3
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-11 18:35:29 -0500

    Time: 2 ms (80.54%) | Memory: 19.3 MB (40.94%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,49 +1,54 @@
+from collections import deque
+
 class Solution:
-    def spread(self, a, i, j, rotAt, currTime):
+    def spread(self, a, i, j, q):
         l, r, t, b = j-1, j+1, i-1, i+1
         didSpread = False
 
         if l >= 0 and a[i][l] == 1:
-            rotAt[i][l] = currTime+1 
             a[i][l] = 2
+            q.appendleft((i,l))
             didSpread = True
 
         if t >= 0 and a[t][j] == 1:
-            rotAt[t][j] = currTime+1 
             a[t][j] = 2
+            q.appendleft((t,j))
             didSpread = True
 
         if r < len(a[0]) and a[i][r] == 1:
-            rotAt[i][r] = currTime+1 
             a[i][r] = 2
+            q.appendleft((i,r))
             didSpread = True
 
         if b < len(a) and a[b][j] == 1:
-            rotAt[b][j] = currTime+1 
             a[b][j] = 2
+            q.appendleft((b,j))
             didSpread = True
 
         return didSpread
 
     def orangesRotting(self, a: List[List[int]]) -> int:
-        m, n = len(a), len(a[0])
-        rotAt = [[0]*n for _ in range(m)]
+        m, n = len(a), len(a[0])  
+        q = deque()
+        # visited = [[0]*n for _ in range(m)]
+
         currTime = 0
-        found = True
-        didSpread = True
 
-        while found and didSpread:
+        for i in range(m):
+            for j in range(n):
+                if a[i][j] == 2:
+                    q.appendleft((i,j))
 
-            found = False
+        while len(q)!=0:
+            currLen = len(q)
             didSpread = False
-            for i in range(m):
-                for j in range(n):
-                    if a[i][j] == 2 and rotAt[i][j] == currTime:
-                        found = True
-                        if self.spread(a, i, j, rotAt, currTime):
-                            didSpread = True
-            
-            if found and didSpread:
+
+            for _ in range(currLen):
+                (i, j) = q.pop()
+                if self.spread(a, i, j, q):
+                    didSpread = True
+
+            if didSpread:
                 currTime += 1
 
         for i in range(m):
