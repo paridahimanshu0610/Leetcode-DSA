efec9234bc2477747502eb4d33f97fbcf67156d0
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-11 15:11:17 -0500

    Time: 442 ms (22.48%) | Memory: 42.2 MB (22.99%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -3,6 +3,9 @@ class Solution:
         if idx >= len(a):
             return 0
 
+        if dp[idx][toBuy] is not None:
+            return dp[idx][toBuy]
+             
         if toBuy == 1:
             totalProfit = max(-a[idx] + self.fromThisDay(a, idx+1, 0, fee, dp), self.fromThisDay(a, idx+1, 1, fee, dp))
         else:
@@ -14,18 +17,8 @@ class Solution:
 
     def maxProfit(self, a: List[int], fee: int) -> int:
         n = len(a)
-        dp = [0]*2
+        idx, toBuy = 0, 1
 
-        for idx in range(n-1,-1,-1):
-            temp = [None]*2
-            for toBuy in range(2):
-                if toBuy == 1:
-                    totalProfit = max(-a[idx] + dp[0], dp[1])
-                else:
-                    totalProfit = max(a[idx]-fee + dp[1], dp[0])
-                
-                temp[toBuy] = totalProfit
-                
-            dp = temp
+        dp = [[None]*2 for _ in range(n)]
 
-        return dp[1] 
+        return self.fromThisDay(a, idx, toBuy, fee, dp)
