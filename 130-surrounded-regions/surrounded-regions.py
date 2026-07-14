623134e8483b1ce7a0b4cbf2d6408d12620de022
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-14 18:38:47 -0500

    Time: 7 ms (51.31%) | Memory: 22.7 MB (17.40%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -4,30 +4,39 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         m, n = len(a), len(a[0])
+        visited = [[0]*n for _ in range(m)]
 
-        def dfs(i, j):
-            if i < 0 or i >= m or j < 0 or j >= n:
+        def dfs(ii, jj):
+            if (ii<0 or ii>=m) or (jj<0 or jj>=n):
                 return
-            if a[i][j] != "O":
-                return
-            a[i][j] = "S"  # mark safe
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
 
-        # 1. Run DFS from every border 'O' to mark safe regions
+            if visited[ii][jj] or (a[ii][jj] == "X"):
+                return
+
+            if a[ii][jj] == "O":
+                visited[ii][jj] = 1
+                a[ii][jj] = "B"
+
+            for i, j in [(ii+1,jj), (ii-1,jj), (ii,jj+1), (ii,jj-1)]:
+                dfs(i, j) 
+
         for i in range(m):
-            dfs(i, 0)
-            dfs(i, n-1)
-        for j in range(n):
-            dfs(0, j)
-            dfs(m-1, j)
+            if (not visited[i][0]) and (a[i][0] == "O"):
+                dfs(i, 0)
 
-        # 2. Flip remaining O -> X, and restore S -> O
+            if (not visited[i][n-1]) and (a[i][n-1] == "O"):
+                dfs(i, n-1)
+
+        for j in range(n):
+            if (not visited[0][j]) and (a[0][j] == "O"):
+                dfs(0, j)
+
+            if (not visited[m-1][j]) and (a[m-1][j] == "O"):
+                dfs(m-1, j)
+
         for i in range(m):
             for j in range(n):
-                if a[i][j] == "O":
-                    a[i][j] = "X"
-                elif a[i][j] == "S":
+                if a[i][j] == "B":
                     a[i][j] = "O"
+                elif a[i][j] == "O":
+                    a[i][j] = "X"
