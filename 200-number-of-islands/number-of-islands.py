73560c9be2d473eaa44d36e90d6bdac44cfbb1cd
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-17 19:08:53 -0500

    Time: 223 ms (97.51%) | Memory: 22.3 MB (29.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,23 @@
 class Solution:
-    def inRange(self, i, j, m, n):
-        return (0 <= i < m) and (0 <= j < n)
-    
-    def traverse(self, a, i, j, visited):
-        visited[i][j] = 1
-        neighbours = [(i+1, j), (i-1, j), (i, j-1), (i, j+1)]
-
-        for x, y in neighbours:
-            if self.inRange(x, y, len(a), len(a[0])) and (a[x][y] == "1") and (not visited[x][y]):
-                self.traverse(a, x, y, visited)
-
     def numIslands(self, a: List[List[str]]) -> int:
         m, n = len(a), len(a[0])
         visited = [[0]*n for _ in range(m)]
+
+        def dfs(ii, jj):
+            if (ii<0 or ii>=m) or (jj<0 or jj>=n) or a[ii][jj] == "0" or visited[ii][jj]:
+                return
+
+            visited[ii][jj] = 1
+
+            for i,j in [(ii+1,jj), (ii-1,jj), (ii,jj+1), (ii,jj-1)]:
+                dfs(i,j)            
+
         res = 0
 
         for i in range(m):
             for j in range(n):
-                if (a[i][j] == "1") and (not visited[i][j]):
+                if (a[i][j] == "1") and not visited[i][j]:
                     res += 1
-                    self.traverse(a, i, j, visited)
+                    dfs(i, j)
 
         return res
