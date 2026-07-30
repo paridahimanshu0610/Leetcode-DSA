1903a8dacd570b11138618bafd3a2705cf8a8893
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-30 12:26:58 -0500

    Time: 48 ms (54.49%) | Memory: 28.1 MB (38.54%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,39 +1,35 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n = len(graph)
-        termNodes = set()
-        safeNode = {i:False for i in range(n)}
+        state = {i:0 for i in range(n)} # 0->unvisited, 1->visiting, 2->safe, 3->unsafe
 
         for i, edges in enumerate(graph):
             if len(edges) == 0:
-                termNodes.add(i)
-                safeNode[i] = True
-
-        visited = [0]*n
+                state[i] = 2
 
         def isSafe(curr):
-            visited[curr] = 1
+            state[curr] = 1
 
-            if safeNode[curr]:
+            if state[curr] == 2:
                 return True
 
             allSafe = True
 
             for nv in graph[curr]:
-                if not visited[nv]:
+                if not state[nv]:
                     allSafe = allSafe and isSafe(nv)
                 else:
-                    allSafe = allSafe and safeNode[nv]
+                    allSafe = allSafe and (state[nv] == 2)
 
                 if not allSafe:
+                    state[curr] = 3
                     return False
 
-            safeNode[curr] = True
-
+            state[curr] = 2
             return True
 
         for i in range(n):
-            if not visited[i]:
+            if not state[i]:
                 isSafe(i)
 
-        return [key for key, value in safeNode.items() if value]
+        return [key for key, value in state.items() if value==2]
